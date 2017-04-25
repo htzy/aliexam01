82c8c6d00d190bac0495ef70c592866604565b15
htzy <ShiheHuang@foxmail.com>
2017-04-25 21:54:32 +0800

    better codes

--- src/main/scala/com/huangshihe/logisticregression/EmailLogisticRegression.scala
+++ src/main/scala/com/huangshihe/logisticregression/EmailLogisticRegression.scala
@@ -44,6 +44,7 @@ object EmailLogisticRegression {
 
     }
 
+    @Deprecated
     def useMLLib(): Unit = {
         // 创建一个HashingTF实例把邮件文本映射为包含10000个特征的向量
         val tf = new org.apache.spark.mllib.feature.HashingTF(numFeatures = 10000)
@@ -51,23 +52,26 @@ object EmailLogisticRegression {
         val spamFeatures = spam.map(email => tf.transform(email.split(" ")))
         val normalFeatures = normal.map(email => tf.transform(email.split(" ")))
         // 创建LabeledPoint数据集分别存放阳性（垃圾邮件）和阴性（正常邮件）的例子
-        val positiveExamples = spamFeatures.map(features=> org.apache.spark.mllib.regression.LabeledPoint(1, features))
-        val negativeExamples = normalFeatures.map(features=>org.apache.spark.mllib.regression.LabeledPoint(0, features))
+        val positiveExamples = spamFeatures.map(features => org.apache.spark.mllib.regression.LabeledPoint(1, features))
+        val negativeExamples = normalFeatures.map(features => org.apache.spark.mllib.regression.LabeledPoint(0, features))
         val trainingData = positiveExamples.union(negativeExamples)
-        trainingData.cache()// 因为逻辑回归是迭代算法，所以缓存训练数据rdd
+        trainingData.cache()
+        // 因为逻辑回归是迭代算法，所以缓存训练数据rdd
         // 使用SGD算法运行逻辑回归
         val model = new org.apache.spark.mllib.classification.LogisticRegressionWithSGD().run(trainingData)
 
         // 以阳性（垃圾邮件）和阴性（正常邮件）的例子分别进行测试
         val posTest = tf.transform("omg get cheap stuff by sending money to...".split(" "))
         val negTest = tf.transform("Hi Dad, I started studying Spark the other ...".split(" "))
-        println("Prediction for positive test example:"+model.predict(posTest))
-        println("Prediction for negative test example:"+model.predict(negTest))
+        println("Prediction for positive test example:" + model.predict(posTest))
+        println("Prediction for negative test example:" + model.predict(negTest))
 
     }
 
     def main(args: Array[String]): Unit = {
         // Spark2.0 推荐使用ml而不是mllib
         useMLLib()
+
+        spark.stop()
     }
 }
