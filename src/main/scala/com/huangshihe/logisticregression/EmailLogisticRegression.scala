1d4a06ae2299a022178d2f952ab0ab6c2d655d6e
htzy <ShiheHuang@foxmail.com>
2017-04-25 16:49:19 +0800

    增加注释

--- src/main/scala/com/huangshihe/logisticregression/EmailLogisticRegression.scala
+++ src/main/scala/com/huangshihe/logisticregression/EmailLogisticRegression.scala
@@ -16,7 +16,11 @@ object EmailLogisticRegression {
         val spam: RDD[String] = spark.sparkContext.textFile("src/main/resources/data/email_spam.txt")
         val ham: RDD[String] = spark.sparkContext.textFile("src/main/resources/data/email_normal.txt")
 
+        // 首先使用分解器Tokenizer把句子划分为单个词语
         val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
+        // 对于每个句子（词袋），使用HashingTF将句子转换为特征向量，最后使用IDF重新调整特征向量。这种转换通常可以提高使用文本特征的性能。
+
+        // 开启隐式转换，否则rdd不能直接调用toDF转为DataFrame
         import spark.implicits._
 
         tokenizer.transform(spam.toDF("sentence")).take(10).foreach(println)
