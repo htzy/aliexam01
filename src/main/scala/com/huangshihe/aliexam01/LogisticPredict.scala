06fa22fde3cd56a362e3873813af22b4cdd42122
htzy <ShiheHuang@foxmail.com>
2017-05-08 00:31:10 +0800

    基本构建好step 1中的模型，需要找更好的测试数据集。

--- src/main/scala/com/huangshihe/aliexam01/LogisticPredict.scala
+++ src/main/scala/com/huangshihe/aliexam01/LogisticPredict.scala
@@ -3,9 +3,12 @@ package com.huangshihe.aliexam01
 import java.text.{DateFormat, SimpleDateFormat}
 import java.util.Date
 
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.types.IntegerType
-import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession, functions}
 
 /**
   * Created by huangshihe on 05/05/2017.
@@ -28,16 +31,16 @@ object LogisticPredict {
       * 行为类型：浏览-1，收藏-2，加购物车-3，购买-4
       * 模型思路：
       * 根据行为类型将数据分为四类，1-3类中的记录若在4中出现，即成功购买的标注1，否则标注0。
-      * 数据输入列：user_id,item_id,time,oper_1,oper_2,oper_3 (oper_1即为否浏览过；oper_2为是否收藏；oper_3为是否加入购物车)
-      * 自变量：time,oper_1,oper_2,oper_3
-      * 因变量：oper_4
+      * 数据输入列：user_id,item_id,time,type_1,type_2,type_3 (type_1即为否浏览过；type_2为是否收藏；type_3为是否加入购物车)
+      * 自变量：time,type_1,type_2,type_3
+      * 因变量：type_4
       *
       * ----------------------
       * TODO step 2
-      * 自变量增加oper_1count,oper_2count,oper_3count (即为浏览过的次数，收藏的次数，加入购物车的次数)
+      * 自变量增加type_1count,type_2count,type_3count (即为浏览过的次数，收藏的次数，加入购物车的次数)
       * ----------------------
       * TODO step 3
-      * 考虑是否有oper_1和oper_1count同时存在的必要性，或者只用哪一个更好？同理如行为类型2、3。使用模型验证。
+      * 考虑是否有type_1和type_1count同时存在的必要性，或者只用哪一个更好？同理如行为类型2、3。使用模型验证。
       * ----------------------
       * TODO step 4
       * 自变量加入用户位置的空间标识
@@ -73,13 +76,76 @@ object LogisticPredict {
         wholeData.take(10).foreach(println)
 
         // 将数据按行为类型分成4类
-        val dataOfView = wholeData.filter(row => row.getAs[Int]("behavior_type") == VIEW_TYPE)
-        val dataOfCollect = wholeData.filter(row => row.getAs[Int]("behavior_type") == COLLECT_TYPE)
-        val dataOfAdd = wholeData.filter(row => row.getAs[Int]("behavior_type") == ADD_TO_CART_TYPE)
+        // 浏览的记录：user_id,item_id,time
+        val dataOfView = wholeData.filter(row => row.getAs[Int]("behavior_type") == VIEW_TYPE).drop("behavior_type")
+        // 收藏的记录：user_id,item_id,time
+        val dataOfCollect = wholeData.filter(row => row.getAs[Int]("behavior_type") == COLLECT_TYPE).drop("behavior_type")
+        // 加购物车的记录：user_id,item_id,time
+        val dataOfAdd = wholeData.filter(row => row.getAs[Int]("behavior_type") == ADD_TO_CART_TYPE).drop("behavior_type")
+        // 成功购买的记录：user_id,item_id,time,label=1.0
         val dataOfBuy = wholeData.filter(row => row.getAs[Int]("behavior_type") == BUY_TYPE)
+            .drop("behavior_type").withColumn("label", functions.lit(1.0))
+
+        // join（通过(user_id,item_id,time)进行join操作）已成功购买的记录，加上label=1.0
+        val positiveView = dataOfView.join(dataOfBuy, Seq("user_id", "item_id", "time"))
+        // 实践证明：不要随便用差集，贼慢！
+        // 浏览的记录与标记为1.0的浏览的记录的差集即为浏览了但是没有生成购买记录的记录
+        val negativeView = dataOfView.except(positiveView.drop("label")).withColumn("label", functions.lit(0.0))
+        val allView = positiveView.union(negativeView)
+            .withColumn("type_1", functions.lit(true))
+            .withColumn("type_2", functions.lit(false)).withColumn("type_3", functions.lit(false))
+
+        val positiveCollect = dataOfCollect.join(dataOfBuy, Seq("user_id", "item_id", "time"))
+        val negativeCollect = dataOfCollect.except(positiveCollect.drop("label")).withColumn("label", functions.lit(0.0))
+        val allCollect = positiveCollect.union(negativeCollect)
+            .withColumn("type_2", functions.lit(true))
+            .withColumn("type_1", functions.lit(false)).withColumn("type_3", functions.lit(false))
+
+        val positiveAdd = dataOfAdd.join(dataOfBuy, Seq("user_id", "item_id", "time"))
+        val negativeAdd = dataOfAdd.except(positiveAdd.drop("label")).withColumn("label", functions.lit(0.0))
+        val allAdd = positiveAdd.union(negativeAdd)
+            .withColumn("type_3", functions.lit(true))
+            .withColumn("type_1", functions.lit(false)).withColumn("type_2", functions.lit(false))
 
         // 预处理后，将1-3类数据合并
+        val data = allView union allCollect union allAdd
 
+        //        data.show(20)
+
+        // 生成特征值
+        val vectorAssembler = new VectorAssembler().setInputCols(Array("time", "type_1", "type_2", "type_3"))
+            .setOutputCol("features")
+
+        //        val features = vectorAssembler.transform(data)
+        //        features.show(10)
+
+        // 使用逻辑回归
+        val lor = new LogisticRegression()
+        // 新建pipeline，分为三个阶段：分词、转换特征向量和逻辑回归
+        val pipeline = new Pipeline().setStages(Array(vectorAssembler, lor))
+        // 训练模型
+        val model = pipeline.fit(data)
+
+        import spark.implicits._
+        val test = Seq((10001082, 53616768, 1, 0, 0, 1)
+            , (10001082, 53616768, 1, 1, 0, 0)
+            , (10001082, 53616768, 1, 0, 1, 0)
+            , (10001082, 53616768, 2, 0, 0, 1)
+            , (10001082, 290088061, 1, 1, 0, 0)
+            , (10001082, 323339743, 1, 0, 1, 0)
+        ).toDF("user_id", "item_id", "time", "type_1", "type_2", "type_3")
+        model.transform(test).show()
+
+        //        +--------+---------+----+------+------+------+-----------------+--------------------+--------------------+----------+
+        //        | user_id|  item_id|time|type_1|type_2|type_3|         features|       rawPrediction|         probability|prediction|
+        //        +--------+---------+----+------+------+------+-----------------+--------------------+--------------------+----------+
+        //        |10001082| 53616768|   1|     0|     0|     1|[1.0,0.0,0.0,1.0]|[2.54039847655302...|[0.92692582171709...|       0.0|
+        //        |10001082| 53616768|   1|     1|     0|     0|[1.0,1.0,0.0,0.0]|[2.54039847655302...|[0.92692582171709...|       0.0|
+        //        |10001082| 53616768|   1|     0|     1|     0|[1.0,0.0,1.0,0.0]|[2.54039847655302...|[0.92692582171709...|       0.0|
+        //        |10001082| 53616768|   2|     0|     0|     1|[2.0,0.0,0.0,1.0]|[2.54062936941713...|[0.92694145955193...|       0.0|
+        //        |10001082|290088061|   1|     1|     0|     0|[1.0,1.0,0.0,0.0]|[2.54039847655302...|[0.92692582171709...|       0.0|
+        //        |10001082|323339743|   1|     0|     1|     0|[1.0,0.0,1.0,0.0]|[2.54039847655302...|[0.92692582171709...|       0.0|
+        //        +--------+---------+----+------+------+------+-----------------+--------------------+--------------------+----------+
 
     }
 
