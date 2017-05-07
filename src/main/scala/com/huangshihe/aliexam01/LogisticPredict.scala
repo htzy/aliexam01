1f1250df9250e270880472255d2f7ef0de1b7447
htzy <ShiheHuang@foxmail.com>
2017-05-07 20:02:05 +0800

    使用三种方法进行DataFrame schema 转换:udf;withColumn+cast;select+cast+as

--- src/main/scala/com/huangshihe/aliexam01/LogisticPredict.scala
+++ src/main/scala/com/huangshihe/aliexam01/LogisticPredict.scala
@@ -26,35 +26,48 @@ object LogisticPredict {
       */
     def getPredictData(): Unit = {
         // 将数据转换格式
+        // ref: http://stackoverflow.com/questions/29383107/how-to-change-column-types-in-spark-sqls-dataframe
         df.printSchema()
-//        root
-//        |-- user_id: string (nullable = true)
-//        |-- item_id: string (nullable = true)
-//        |-- behavior_type: string (nullable = true)
-//        |-- user_geohash: string (nullable = true)
-//        |-- item_category: string (nullable = true)
-//        |-- time: string (nullable = true)
+        //        root
+        //        |-- user_id: string (nullable = true)
+        //        |-- item_id: string (nullable = true)
+        //        |-- behavior_type: string (nullable = true)
+        //        |-- user_geohash: string (nullable = true)
+        //        |-- item_category: string (nullable = true)
+        //        |-- time: string (nullable = true)
 
+        // 以下方法中df的schema都不会发生变化
+        // 方法一：使用org.apache.spark.sql.functions.udf + withColumn
         import org.apache.spark.sql.functions._
         val toInt = udf[Int, String](_.toInt)
         val changed = df.withColumn("user_id", toInt(df("user_id")))
         changed.printSchema()
-//        root
-//        |-- user_id: integer (nullable = true)
-//        |-- item_id: string (nullable = true)
-//        |-- behavior_type: string (nullable = true)
-//        |-- user_geohash: string (nullable = true)
-//        |-- item_category: string (nullable = true)
-//        |-- time: string (nullable = true)
+        //        root
+        //        |-- user_id: integer (nullable = true)
+        //        |-- item_id: string (nullable = true)
+        //        |-- behavior_type: string (nullable = true)
+        //        |-- user_geohash: string (nullable = true)
+        //        |-- item_category: string (nullable = true)
+        //        |-- time: string (nullable = true)
         println("df:")
         df.printSchema()
-//        root
-//        |-- user_id: string (nullable = true)
-//        |-- item_id: string (nullable = true)
-//        |-- behavior_type: string (nullable = true)
-//        |-- user_geohash: string (nullable = true)
-//        |-- item_category: string (nullable = true)
-//        |-- time: string (nullable = true)
+        //        root
+        //        |-- user_id: string (nullable = true)
+        //        |-- item_id: string (nullable = true)
+        //        |-- behavior_type: string (nullable = true)
+        //        |-- user_geohash: string (nullable = true)
+        //        |-- item_category: string (nullable = true)
+        //        |-- time: string (nullable = true)
+        // 方法二：用withColumn + cast
+        df.withColumn("user_id", df("user_id").cast(IntegerType)).printSchema()
+        println("df:")
+        df.printSchema() // schema保持不变
+
+        // 方法三：用select + cast + as
+        df.select(df("user_id").cast(IntegerType).as("user_id"), df("item_id").cast(IntegerType).as("item_id")).printSchema()
+        //        root
+        //        |-- user_id: integer (nullable = true)
+        //        |-- item_id: integer (nullable = true)
     }
 
     val VIEW_TYPE = 1
